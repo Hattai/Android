db775c757c09807099a96ed4493efefa43684885
Emerson Gogola <egogola@scopus.com.br>
2016-08-30 17:54:59 -0300

    Project BrowserPrimitivo, first version.

---
+++ BrowserPrimitivo/app/src/main/java/br/com/egogola/browserprimitivo/MainActivity.java
@@ -0,0 +1,63 @@
+package br.com.egogola.browserprimitivo;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtUrl;
+    private WebView webView;
+    private ProgressBar pgr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtUrl = (EditText) findViewById(R.id.txtUrl);
+        webView = (WebView) findViewById(R.id.webView);
+        pgr = (ProgressBar) findViewById(R.id.pgr);
+
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                pgr.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                pgr.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    public void btnGoOnClick(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+
+        String url = "http://" + txtUrl.getText().toString();
+        webView.loadUrl(url);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
